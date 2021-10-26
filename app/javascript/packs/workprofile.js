79ad69a6ff2743440c8af6c5c30612180dcd5d2f
JustaFramework <wachirawamunyi@gmail.com>
2021-10-27 01:14:51 +0300

    removed deprecated listeners

--- app/javascript/packs/workprofile.js
+++ app/javascript/packs/workprofile.js
@@ -1,5 +1,5 @@
-$(document).ready(function($) {
-	$('#Ongoing_work').click(function () {
+document.addEventListener("DOMContentLoaded", function(){	
+	$('#Ongoing_work').on('click', function () {
 		const checkBox = document.getElementById('Ongoing_work');
 		const endDate = document.getElementById('end_date')
 		if (checkBox.checked) {
